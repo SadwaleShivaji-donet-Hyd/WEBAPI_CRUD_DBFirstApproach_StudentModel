e74fdb65f3745cd185655990438a6e4efd858899
Sadwale.Shivaji <Sadwale.Shivaji@ENCDAPHYDLT0095>
2025-10-08 12:49:14 +0530

    Add project files.

---
+++ WEBAPI_CRUD_DBFirstApproach_StudentModel/Models/DbfirstContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WEBAPI_CRUD_DBFirstApproach_StudentModel.Models;
+
+public partial class DbfirstContext : DbContext
+{
+    public DbfirstContext()
+    {
+    }
+
+    public DbfirstContext(DbContextOptions<DbfirstContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=ENCDAPHYDLT0095;database=DBFirst;Integrated Security=true;Trusted_Connection=true;TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Student__3214EC077BD5CACD");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Department)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(25)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ WEBAPI_CRUD_DBFirstApproach_StudentModel/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WEBAPI_CRUD_DBFirstApproach_StudentModel.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Department { get; set; }
+
+    public int? Marks { get; set; }
+}
